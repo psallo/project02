b273868c47c515d3ac73f8f6942abc90e3642e3c
psallo <treasure0613@gmail.com>
2025-11-22 22:14:57 +0900

    footer 추가

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,21 @@
+import styled from "@emotion/styled";
 import { Typography } from "@mui/material";
 
 export default function Footer() {
-  return <Typography>footer</Typography>;
+  return (
+    <Container>
+      <Typography variant="caption" sx={{ color: "white" }}>
+        © 2025 All Rights Reserved
+      </Typography>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: #2e4f21;
+  align-items: flex-end;
+  justify-content: center;
+  height: 50px;
+`;
